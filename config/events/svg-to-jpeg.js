682a10e670e6200b45a9aaafd5a253472e50ab1d
Lea Rosema <lea@lea.codes>
2023-12-09 10:51:58 +0100

    feat: ace preview images

--- config/events/svg-to-jpeg.js
+++ config/events/svg-to-jpeg.js
@@ -1,28 +1,33 @@
 // https://bnijenhuis.nl/notes/automatically-generate-open-graph-images-in-eleventy/
 // concerts SVG to JPEG for open graph images
 
-const fs = require('fs');
+const fs = require('fs/promises');
 const Image = require('@11ty/eleventy-img');
+const { glob } = require('glob');
+const path = require('path');
 
-module.exports = function () {
+module.exports = async function () {
   const socialPreviewImagesDir = 'dist/assets/images/social-preview/';
-  fs.readdir(socialPreviewImagesDir, (err, files) => {
-    if (!!files && files.length > 0) {
-      files.forEach((fileName) => {
-        if (fileName.endsWith('.svg')) {
-          let imageUrl = socialPreviewImagesDir + fileName;
-          Image(imageUrl, {
-            formats: ['jpeg'],
-            outputDir: './' + socialPreviewImagesDir,
-            filenameFormat: function (id, src, width, format, options) {
-              let outputFileName = fileName.substring(0, fileName.length - 4);
-              return `${outputFileName}.${format}`;
-            },
-          });
-        }
-      });
-    } else {
-      console.log('⚠ No social images found');
+  const files = await glob([path.join(socialPreviewImagesDir, '*.svg'), path.join(socialPreviewImagesDir, '**/*.svg')]);
+  if (!files || files.length === 0) {
+    console.log('⚠ No social images found');
+    return;
+  }
+  for (const inputFileName of files) {
+    if (!inputFileName.endsWith('-preview.svg')) {
+      continue;
     }
-  });
+    const outputDir = path.dirname(inputFileName);
+
+    Image(inputFileName, {
+      formats: ['jpeg'],
+      outputDir,
+      filenameFormat: function (id, src, width, format, options) {
+        const outputFileName = `${path.basename(inputFileName)}.${format}`
+        return outputFileName;
+      },
+    });
+  }
+
+
 };
